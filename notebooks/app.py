8070d81bde9199cc1519547b345d85e28262d2d3
Daria Ponomareva <dponomareva@slb.com>
2022-12-05 17:37:04 +0400

    app

--- notebooks/app.py
+++ notebooks/app.py
@@ -9,3 +9,15 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+xx = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x) = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
